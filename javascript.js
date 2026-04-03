2d299672ad920724860c8dd5aaa421b9edabdc9a
fallinblossoms777 <166082974+fallinblossoms777@users.noreply.github.com>
2026-04-03 16:57:49 -0300

    Create function that randomly returns rock, paper, or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice () {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if (choice === 1) {
+        return "Paper";
+    } else if (choice === 2) {
+        return "Scissors";
+    } else {
+        return "Rock";
+    }
+}
