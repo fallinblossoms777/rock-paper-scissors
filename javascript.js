839105c9bc131b91db1817266a4ba77840325ebb
fallinblossoms777 <166082974+fallinblossoms777@users.noreply.github.com>
2026-04-03 17:39:34 -0300

    Add function to determine winner and award 10 points

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     if (choice === 1) {
-        return "Paper";
+        return "paper";
     } else if (choice === 2) {
-        return "Scissors";
+        return "scissors";
     } else {
-        return "Rock";
+        return "rock";
     }
 }
 
@@ -13,5 +13,23 @@ function getHumanChoice() {
     return prompt("Choose 'rock', 'paper', or 'scissors': ");
 }
 
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return console.log("It's a tie");
+    } else if (humanChoice === "rock" && computerChoice === "scissors" 
+        || humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 10; 
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 10; 
+    }
+}
+    
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = getHumanChoice().toLowerCase();
+let computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice); 
